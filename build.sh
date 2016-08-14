bca19121a3940384818c7300dcc06e29ef1d81f3
Doug Davis <dug@us.ibm.com>
2016-08-14 12:31:40 -0700

    init
    Signed-off-by: Doug Davis <dug@us.ibm.com>

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-docker build -f Dockerfile.build -t tinybuild:alpine .
-docker create --name result tinybuild:alpine .
-docker cp result:/hello .
-docker rm -f result
-docker rmi -f tinybuild:alpine
-docker build -t tiny .
-docker run --rm tiny
